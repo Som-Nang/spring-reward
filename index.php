172a18ea0ae1b14faac6ba6fb23dbd05118f0ec4
Som-Nang <somnangmega13@gmail.com>
2023-12-23 23:14:46 +0700

    select by image, dinamic select winner

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+include('dbconnect.php');
+
+?>
+
 <!DOCTYPE html>
 <html data-theme="dracula">
 
@@ -9,6 +15,8 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" />
   <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet" />
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <script src="./main.js"></script>
 </head>
 
 <body>
@@ -51,21 +59,42 @@
       <main class="relative z-0 flex-1 pb-8 px-6 bg-gray-600">
         <div class="grid pb-10  mt-4">
           <!-- Start Content-->
+
           <div class="w-full flex space-x-2 justify-between relative">
-            <select class="select select-info w-full ">
-              <option disabled selected>Select Prize</option>
-              <option>Power Bank</option>
-              <option>Cup</option>
-              <option>Phone</option>
-            </select>
-            <select class="select select-info max-w-xs w-full ">
-              <option disabled selected>Select Winner</option>
-              <option>1</option>
+
+
+            <select id="winnerSelector" class="select select-info max-w-xs w-full ">
+              <option selected disabled> Select Winners </option>
+              <option> 1 </option>
               <option>2</option>
               <option>3</option>
               <option>4</option>
               <option>5</option>
             </select>
+
+            <select id="prizeSelect" class="select select-info w-full ">
+              <option disabled selected>Select Prize</option>
+              <?php $sql = "SELECT tblprize.id ,tblprize.prizeName,tblprize.prizePic FROM tblprize";
+              $query = $dbh->prepare($sql);
+              $query->execute();
+              $results = $query->fetchAll(PDO::FETCH_OBJ);
+              $cnt = 1;
+              if ($query->rowCount() > 0) {
+                foreach ($results as $row) {
+
+              ?>
+                  <option value="<?php echo $row->id; ?>" data-img="<?php echo htmlentities($row->prizePic); ?>" data-name="<?php echo htmlentities($row->prizeName); ?>">
+                    <?php echo htmlentities($row->prizeName); ?>
+                  </option>
+              <?php
+
+                }
+              }
+              ?>
+
+
+            </select>
+
             <button class="btn btn-active btn-success">Save</button>
           </div>
 
@@ -73,10 +102,10 @@
             <div class="prize w-96 m-2">
               <div class="card w-96 bg-base-100 shadow-xl">
                 <figure class="px-5 pt-10">
-                  <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+                  <img id="prizeImage" src="./image/qs.jpg" alt="Select Items First!" class="rounded-xl" />
                 </figure>
                 <div class="card-body items-center text-center">
-                  <h2 class="card-title ">Power Bank</h2>
+                  <p id="prizeName" class="card-title"></p>
 
                 </div>
               </div>
@@ -87,84 +116,60 @@
             </div>
             <!-- Winner -->
             <div class=" m-2 w-full">
-              <div class="w-full flex flex-wrap justify-center space-x-4 space-y-4 items-center">
-
+              <div id="cardContainer" class="w-full flex flex-wrap justify-center space-x-4 space-y-4 items-center">
                 <div class="card w-60 h-60 bg-base-100 shadow-xl">
-
-                  <img class="w-full h-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
-
+                  <img class="w-full h-full" src="./image/qs.jpg" alt="Shoes" class="rounded-xl" />
                   <div class="py-2 flex flex-col justify-center items-center text-center">
-                    <p class="card-title ">Name: John</p>
-                    <p class="card-title ">ID: B20125</p>
-
+                    <p class="card-title">Winner Name</p>
+                    <p class="card-title">Winner ID</p>
                   </div>
                 </div>
-
-                <div class="card w-60 h-60 bg-base-100 shadow-xl">
-
-                  <img class="w-full h-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
-
-                  <div class="py-2 flex flex-col justify-center items-center text-center">
-                    <p class="card-title ">Name: John</p>
-                    <p class="card-title ">ID: B20125</p>
-
-                  </div>
-                </div>
-
-                <div class="card w-60 h-60 bg-base-100 shadow-xl">
-
-                  <img class="w-full h-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
-
-                  <div class="py-2 flex flex-col justify-center items-center text-center">
-                    <p class="card-title ">Name: John</p>
-                    <p class="card-title ">ID: B20125</p>
-
-                  </div>
-                </div>
-
-                <div class="card w-60 h-60 bg-base-100 shadow-xl">
-
-                  <img class="w-full h-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
-
-                  <div class="py-2 flex flex-col justify-center items-center text-center">
-                    <p class="card-title ">Name: John</p>
-                    <p class="card-title ">ID: B20125</p>
-
-                  </div>
-                </div>
-
-                <div class="card w-60 h-60 bg-base-100 shadow-xl">
-
-                  <img class="w-full h-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
-
-                  <div class="py-2 flex flex-col justify-center items-center text-center">
-                    <p class="card-title ">Name: John</p>
-                    <p class="card-title ">ID: B20125</p>
-
-                  </div>
-                </div>
-
-                <div class="card w-60 h-60 bg-base-100 shadow-xl">
-
-                  <img class="w-full h-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
-
-                  <div class="py-2 flex flex-col justify-center items-center text-center">
-                    <p class="card-title ">Name: John</p>
-                    <p class="card-title ">ID: B20125</p>
-
-                  </div>
-                </div>
-
               </div>
-
             </div>
+
           </div>
-
-
         </div>
-      </main>
+
     </div>
+    </main>
   </div>
+  </div>
+
+
+
+  <script>
+    // Handle change event of the select element
+    document.getElementById('winnerSelector');
+
+    // Trigger change event to generate the initial card
+    document.getElementById('winnerSelector').dispatchEvent(new Event('change'));
+
+    // Handle change event of the select element
+    document.getElementById('winnerSelector').addEventListener('change', function() {
+      // Get the selected value
+      var selectedValue = parseInt(this.value);
+
+      // Clear existing cards
+      document.getElementById('cardContainer').innerHTML = '';
+
+      // Generate cards based on the selected value
+      for (var i = 1; i <= selectedValue; i++) {
+        var cardDiv = document.createElement('div');
+        cardDiv.className = 'card w-60 h-60 bg-base-100 shadow-xl';
+        cardDiv.innerHTML = `
+                <img class="w-full h-full" src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+                <div class="py-2 flex flex-col justify-center items-center text-center">
+                    <p class="card-title">Name: <?php echo htmlentities($row->prizeName); ?></p>
+                    <p class="card-title">ID: B20125</p>
+                </div>
+            `;
+
+        // Append the card to the container
+        document.getElementById('cardContainer').appendChild(cardDiv);
+      }
+    });
+  </script>
+
 </body>
 
 </html>
---
+++ main.js
@@ -0,0 +1,23 @@
+// Change dinamix prize
+
+$(document).ready(function () {
+  // Set the initial image source
+  $("#prizeImage").attr("src", "");
+  $("#prizeName").text("");
+
+  // Handle change event of the select element
+  $("#prizeSelect").change(function () {
+    // Get the selected option
+    var selectedOption = $(this).find(":selected");
+
+    // Get the image source and prize name from the data attributes
+    var imageSrc = selectedOption.data("img");
+    var prizeName = selectedOption.data("name");
+
+    // Update the image source and prize name
+    $("#prizeImage").attr("src", imageSrc);
+    $("#prizeName").text(prizeName);
+  });
+});
+
+// Handle change event of the select element
